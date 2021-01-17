04dd893648599e894ee6eec0adc21a10aea87857
Mahady Manana <60957693+mahady-manana@users.noreply.github.com>
2021-01-17 10:30:36 +0300

    config

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,6 @@
 const port = process.env.PORT || 4000;
 const jwtSecret = process.env.JWT_SECRET || "mysecretkey";
-// const mongouri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/indian-jobs-usrs112fklgd';
-const mongouri = process.env.MONGO_URI || "mongodb+srv://mahady:mahady1906@cluster0.tasds.mongodb.net/mahady?retryWrites=true&w=majority"
+const mongouri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/indian-jobs-usrs112fklgd';
 export default {
     port,
     mongouri,
